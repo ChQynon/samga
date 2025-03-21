8de1d1f2fd5f3ae7cebc786bae1dd7ab984a9be7
qynon <qynon@example.com>
2025-03-21 15:28:25 +0500

    Disable ESLint checks during build

--- next.config.cjs
+++ next.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Включаем статический экспорт
+  eslint: {
+    // Отключаем проверку ESLint во время сборки
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
